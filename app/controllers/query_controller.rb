a184ababb4cc48e078b86264e20fee3da82bef43
Yeti Sno <yeti@yetiz.org>
2015-03-25 00:10:31 +0800

    init

--- app/controllers/query_controller.rb
+++ app/controllers/query_controller.rb
@@ -1,13 +1,13 @@
 class QueryController < ApplicationController
   def update
-
+	  response.headers['Access-Control-Allow-Origin'] = '*'
   end
 
   def create
-
+	response.headers['Access-Control-Allow-Origin'] = '*'
   end
 
   def show
-
+	  response.headers['Access-Control-Allow-Origin'] = '*'
   end
 end
